0c76eb9b3a99a9ccccfe7c8b6b3de4d6e33b1e21
MeatBallFreak69 <glitchclick@gmail.com>
2024-05-07 18:33:50 -0400

    Removed some windows specifics

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ let errorCount = 0;
 
 //The session id for the webcam, used for the command to restart them
 //NOTE * Session ID doesn't seem to change on system reboot
-const enableCam = `pnputil /enable-device "${config.CAM_SESSION_ID}"`; //Command to enable the cam
-const disableCam = `pnputil /disable-device "${config.CAM_SESSION_ID}"`; //Command to disable the cam
+//const enableCam = `pnputil /enable-device "${config.CAM_SESSION_ID}"`; //Command to enable the cam
+//const disableCam = `pnputil /disable-device "${config.CAM_SESSION_ID}"`; //Command to disable the cam
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+/*
 const executeCommand = async (command) =>{
   await exec(command, (error, stdout, stderr) =>{
     if(error){
@@ -30,13 +31,14 @@ const executeCommand = async (command) =>{
     }
     console.log(`Command output: \n${stdout}`);
   });
-}
+}*/
 
+/*
 const restartCam = async () =>{
   await executeCommand(disableCam); //Disables camera first
   await executeCommand(enableCam);  //Then enables it so that it gets rebooted 😂
   errorCount = 0;
-}
+}*/
 
 //Webcam settings
 var opts = {
@@ -54,9 +56,9 @@ var Webcam = NodeWebcam.create( opts );
 //Captures an image from camera every second and sends the data over to the site!
 setInterval(async () => {
     Webcam.capture("images/camOutput", function(err, imageData) {
-      if(errorCount >= config.ERROR_LIMIT && config.REBOOT_CAM){
+      /*if(errorCount >= config.ERROR_LIMIT && config.REBOOT_CAM){
         restartCam();
-      }
+      }*/
       //const imagePath = path.join(__dirname, 'images', 'test_picture');
       if (err) {
         console.log(errorCount);
@@ -109,11 +111,6 @@ io.on('connection', socket => {
       ip: localIpAddress
     });
 })
-
-/*
-setInterval(() =>{
-    io.emit('message', 'Testing connection');
-}, 5000);*/
 
 
 server.listen(config.PORT, '0.0.0.0');
